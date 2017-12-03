e70ffb2364fd154e3cdc995788c4528cbc9e63e5
Djordje Velickovic <djvelickovic234@gmail.com>
2017-12-03 19:21:56 +0100

    Slave refactor

--- slave/slave.ino
+++ slave/slave.ino
@@ -1,118 +1,199 @@
-  
-  #include <Wire.h>
-  #include <math.h>
-  
-  #define WRK  1
-  #define RTR  2
-  #define IDL  3
-  #define BUFFER_SIZE  100
-  
-  const int address = 8;
-  
-  int state = IDL;
-  int percent = 0;
-  long estimated_time = 0;
-  
-  long prime_buffer[BUFFER_SIZE] ;
-  
-  long buffer_length = 0;
-  
-  long start_number = 0;
-  long end_number = 0;
-  long current_number = 0;
 
-  
-  typedef union {
-    long prime_number;
-    byte primen_buffer[4];
-  } prime_number;
-  
-  
-  void setup() {
-    // put your setup code here, to run once:
-    Wire.begin(address);
-    Wire.onReceive(receiveEvent);
-    // Serial.begin(9600);
-  
+#include <Wire.h>
+#include <math.h>
+
+#define SLAVE_WORKING  0
+#define SLAVE_RETURN   1
+#define SLAVE_IDLE     2
+
+#define TASK 0
+#define STAT 1
+#define PERC 2
+#define RSLT 3
+#define TIME 4
+
+#define FAIL 0
+#define OK 1
+
+
+#define BUFFER_SIZE  100
+
+typedef union {
+  long value;
+  byte avalue[4];
+} LongNumber;
+
+const int address = 8;
+
+volatile byte state = SLAVE_IDLE;
+volatile byte percent = 0;
+volatile long eta = 0;
+
+long primeBuffer[BUFFER_SIZE] ;
+
+volatile long bufferSize = 0;
+volatile long bufferPosition = 0;
+
+long lowerBound; // wrap in LongNumber before sending if error with memory!
+long upperBound;
+long currentNumber = 0;
+
+volatile byte lastMessage;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(address);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  Serial.begin(9600);
+
+  lowerBound = 100000;
+  upperBound = 100200;
+  state = SLAVE_WORKING;
+  currentNumber = lowerBound;
+}
+
+void loop() {
+  if (state == SLAVE_WORKING) {
+    if (currentNumber < upperBound) {
+      Serial.print("Checking ");
+      Serial.println(currentNumber);
+      
+      if (isPrime(currentNumber)) {
+        primeBuffer[bufferSize++] = currentNumber;
+        Serial.println(currentNumber);
+      }
+      currentNumber++;
+      
+      double dpercent = 100.0/(upperBound - lowerBound) * (currentNumber - lowerBound);
+      percent = (byte)dpercent;
+      Serial.print("Percent ");
+      Serial.println(percent);
+    }
+    else {
+      state = SLAVE_RETURN;
+      printAllNumbers();
+    }
+  }
+  else { // idle
+    delay(100);
+  }
+}
+
+void printAllNumbers(){
+  for (int i = 0; i < bufferSize; i++){
+    Serial.println(primeBuffer[i]);
+  }
+}
+
+bool isPrime(long number) {
+  if (number % 2 == 0) {
+    return false;
   }
   
-  void loop() {
-    if (state == WRK){
-      if (current_number < end_number){
-        if (isPrime(current_number)){
-          prime_buffer[buffer_length] = current_number;
-          buffer_length++;
-        }
-        current_number++;
-      }
-      else {
-        state = RTR;
-      }
-    }
-    else { // idle
-      delay(100);
-    }
-  }
-
-  bool isPrime(long number){
-//    double number_sqrt = sqrt(number);
-//    long lnsqrt = (long)number_sqrt;
-    if (number % 2 == 0){
+  double number_sqrt = sqrt(number);
+  long lnsqrt = (long)number_sqrt;
+  for (long i = 3; i < lnsqrt; i += 2) {
+    if (number % i == 0) {
       return false;
     }
-    for (long i = 3; i < number-1; i += 2){
-      if (number % i == 0){
-        return false;
+  }
+  return true;
+}
+
+
+
+
+void requestEvent() {
+  switch (lastMessage) {
+    case TASK:
+      if (state == SLAVE_WORKING) {
+        Wire.write(OK);
       }
-    }
-    return true;
+      else if (state == SLAVE_IDLE) {
+        Wire.write(FAIL);
+      }
+      break;
+    case STAT:
+      Wire.write(state);
+      break;
+    case PERC:
+      Wire.write(percent);
+      break;
+    case TIME:
+      Wire.write(eta);
+      break;
+    case RSLT:
+      if (state == SLAVE_RETURN) {
+        if (bufferPosition < bufferSize) {
+          Wire.write(primeBuffer[bufferPosition++]);
+        }
+        else {
+          state = SLAVE_IDLE;
+          bufferPosition = 0;
+          bufferSize = 0;
+          eta = 0;
+          percent = 0;
+        }
+      }
+      break;
+     default:
+     Serial.println("Oh master what are you doing?!");
+  }
+}
+
+void receiveEvent() {
+  String message = "";
+  int avail = Wire.available();
+  if (avail < 4) {
+    //ERROR
+    return;
+  }
+  while (avail > 0) {
+    avail--;
+    message += Wire.read();
   }
 
-  
-  
-  void receiveEvent(){
-    String message = "";
-    int avail = Wire.available();
-    if (avail < 4){
-      //ERROR
-      return;
-    }
-    while (avail > 0){
-      avail--;
-      message += Wire.read();    
-    }
-    
-    if (message.equals("TASK")){ // task
-      // parse numbers
-    }
-    if (message.equals("STAT")){ // state
-      if (state == WRK){
-        Wire.write("WRK");
-      }
-      else if (state == RTR){
-        Wire.write("RTR");
-      }
-      else if (state ==  IDL){
-        Wire.write("IDL");
+  if (message.equals("TASK")) { // task
+    lastMessage = TASK;
+
+    if (Wire.available() != 8) {
+      while (Wire.available()) {
+        Wire.read();
       }
     }
-    else if (message.equals("RSLT")){ // result ?!??!@
-      if (state == RTR){
-        //RETURN NUMBER
-      }
-      else {
-        Wire.write("F");
-      }
-    }
-    else if (message.equals("PERC")){ // percent
-      Wire.write(percent);
-    }
-    else if (message.equals("TIME")){ // time
-      Wire.write(estimated_time);
-    }
-    
-    
+
+    LongNumber tmp;
+    tmp.avalue[0] = Wire.read();
+    tmp.avalue[1] = Wire.read();
+    tmp.avalue[2] = Wire.read();
+    tmp.avalue[3] = Wire.read();
+    lowerBound = tmp.value;
+
+    tmp.avalue[0] = Wire.read();
+    tmp.avalue[1] = Wire.read();
+    tmp.avalue[2] = Wire.read();
+    tmp.avalue[3] = Wire.read();
+    upperBound = tmp.value;
+
+    state = SLAVE_WORKING;
+
   }
+  if (message.equals("STAT")) { // state
+    lastMessage = STAT;
+  }
+  else if (message.equals("RSLT")) { // result ?!??!@
+    lastMessage = RSLT;
+  }
+  else if (message.equals("PERC")) { // percent
+    lastMessage = PERC;
+  }
+  else if (message.equals("TIME")) { // time
+    lastMessage = TIME;
+  }
+
+}
 
 
 
